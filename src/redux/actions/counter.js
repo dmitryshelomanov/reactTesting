116b31a2819f884e013766c7ce975b844db58045
Shelomanov <i.ivanov@idea-promotion.ru>
2017-12-08 12:34:57 +0300

    test(fix): update redux actions

--- src/redux/actions/counter.js
+++ src/redux/actions/counter.js
@@ -1,16 +1,12 @@
 import { COUNTER_MINES, COUNTER_PLUS } from '../types'
 
 
-export const plus = (num) => (dispatch) => {
-  return dispatch({
-    payload: num,
-    type: COUNTER_PLUS,
-  })
-}
+export const plus = (num) => (dispatch) => dispatch({
+  payload: num,
+  type: COUNTER_PLUS,
+})
 
-export const mines = (num) => (dispatch) => {
-  return dispatch({
-    payload: num,
-    type: COUNTER_MINES,
-  })
-}
+export const mines = (num) => (dispatch) => dispatch({
+  payload: num,
+  type: COUNTER_MINES,
+})
