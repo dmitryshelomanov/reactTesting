496977ac612097de515ab46d97f338816a3c04f5
Shelomanov <i.ivanov@idea-promotion.ru>
2017-12-11 13:45:07 +0300

    refactore(component/counter) update test:

--- src/components/counter/index.test.js
+++ src/components/counter/index.test.js
@@ -1,20 +1,13 @@
 import React from 'react'
 import { shallow, render, configure } from 'enzyme'
+import { stub } from 'sinon'
 import Adapter from 'enzyme-adapter-react-16'
 import renderer from 'react-test-renderer';
-import configureStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
-import { plus, mines } from '../../redux/actions/counter'
 import { Counter } from './index'
 import Actionbtn from '../ui/actionBtn'
-import * as types from '../../redux/types'
+
 
 configure({ adapter: new Adapter() })
-
-beforeEach(() => { 
-  const mw = [thunk]
-  global.mockStore = configureStore(mw)
-})
 
 describe('test component Counter', () => {
 
@@ -23,31 +16,27 @@ describe('test component Counter', () => {
   })
 
   it('dispatches plus action', () => {
-    const store = global.mockStore({})
+    const testFn = stub()
     const wrapper = shallow(
       <Counter
-        onIncCounter={() => store.dispatch(plus(1))}
+        onIncCounter={testFn}
       />
     )
+
     wrapper.find(Actionbtn).at(0).simulate('click')
-    expect(store.getActions()[0]).toEqual({
-      type: types.COUNTER_PLUS,
-      payload: 1
-    })
+    expect(testFn.called).toBeTruthy()
   })
 
   it('dispatches mines action', () => {
-    const store = global.mockStore({})
+    const testFn = stub()
     const wrapper = shallow(
       <Counter
-        onDeincCounter={() => store.dispatch(mines(1))}
+        onDeincCounter={testFn}
       />
     )
+
     wrapper.find(Actionbtn).at(1).simulate('click')
-    expect(store.getActions()[0]).toEqual({
-      type: types.COUNTER_MINES,
-      payload: 1
-    })
+    expect(testFn.called).toBeTruthy()
   })
 
   it('shapshot Counter', () => {
