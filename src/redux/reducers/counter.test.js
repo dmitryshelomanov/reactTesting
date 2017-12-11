538277f04cd6f106cd5ba18dbd0e89912464b6de
Shelomanov <i.ivanov@idea-promotion.ru>
2017-12-11 13:31:01 +0300

    refactore(reducers/coun.test): update example

--- src/redux/reducers/counter.test.js
+++ src/redux/reducers/counter.test.js
@@ -5,22 +5,27 @@ import counter, { INITIAL_STATE } from './counter'
 import * as types from '../types'
 import { plus, mines } from '../actions/counter'
 
-beforeEach(() => { 
-  const mw = [thunk]
-  global.mockStore = configureStore(mw)
-})
+
+const mw = [thunk]
+const mockStore = configureStore(mw)
 
 describe('counter testing', () => {
 
   it('counter default state', () => {
     const initialState = INITIAL_STATE
     const actions = {}
+
     expect(counter(initialState, actions)).toEqual(initialState)
   })
 
   it('counter COUNTER_PLUS state', () => {
-    const store = global.mockStore({})
-    store.dispatch(plus(1))
+    const store = mockStore(INITIAL_STATE)
+    const actions = store.dispatch(plus(1))
+    const testFn = counter(store.getState(), actions)
+
+    expect(testFn).toEqual({
+      number: 1
+    })
     expect(store.getActions()[0]).toEqual({
       type: types.COUNTER_PLUS,
       payload: 1
@@ -28,8 +33,13 @@ describe('counter testing', () => {
   })
 
   it('counter COUNTER_MINES state', () => {
-    const store = global.mockStore({})
-    store.dispatch(mines(1))
+    const store = mockStore(INITIAL_STATE)
+    const actions = store.dispatch(mines(1))
+    const testFn = counter(store.getState(), actions)
+
+    expect(testFn).toEqual({
+      number: 1
+    })
     expect(store.getActions()[0]).toEqual({
       type: types.COUNTER_MINES,
       payload: 1
